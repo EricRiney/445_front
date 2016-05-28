0ee0eb1c1cdbbeeb2e8b59a7dc33e382b1b05db5
EricRiney <fails.to.install@gmail.com>
2016-05-28 23:18:04 +0000

    DB built

---
+++ sql/actual.sql
@@ -0,0 +1,21 @@
+Create DATABASE SKI_BORAD_MFG];
+USE SKI_BORAD_MFG];
+
+CREATE TABLE `Employeee` (
+    EmployeeeID              BIGINT(20) UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    EmployeeeFirst         VARCHAR(50),
+    EmployeeeLast         VARCHAR(50),
+    EmployeeePhone        VARCHAR(50),
+    EmployeeeEmail           VARCHAR(50)
+);
+
+CREATE TABLE `Customer` (
+    EmployeeeID             BIGINT(20) UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    CustomerName            VARCHAR(50),
+    CustomerStreet          VARCHAR(50),
+    CustomerCity            VARCHAR(50),
+    CustomerStateProvience  VARCHAR(50),
+    CustomerCountry         VARCHAR(50),
+    CustomerZIP             VARCHAR(50)
+       
+);
